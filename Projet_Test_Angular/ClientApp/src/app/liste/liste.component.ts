f5dd978ebaeb2d5a014a0619f7fd273c528309e5
DorianTardieu <dorian.tardieu@saintmichelannecy.fr>
2024-03-27 16:06:15 +0100

    MAJ

--- Projet_Test_Angular/ClientApp/src/app/liste/liste.component.ts
+++ Projet_Test_Angular/ClientApp/src/app/liste/liste.component.ts
@@ -19,7 +19,7 @@ export class ListeComponent implements OnInit {
       email: 'lucas@example.com',
       phone: '07 23 45 67 89',
       skills: ['Symfony', 'Doctrine', 'Twig'],
-      image: 'https://example.com/lucas_profile.jpg'
+      image: 'https://www.fredericvigier.com/wp-content/uploads/2018/01/photographe-photo-cv-etudiant.jpg'
     },
     {
       id: 2,
@@ -27,7 +27,7 @@ export class ListeComponent implements OnInit {
       email: 'camille@example.com',
       phone: '06 34 56 78 90',
       skills: ['Python', 'Flask', 'SQLAlchemy'],
-      image: 'https://example.com/camille_profile.jpg'
+      image: 'https://media.licdn.com/dms/image/C4D03AQFxiqCy2pAaNw/profile-displayphoto-shrink_800_800/0/1636382758257?e=2147483647&v=beta&t=aexiA-aJOp130h9efFa-J80NEyb3C0kRAZBxtEQsaw4'
     },
     {
       id: 3,
@@ -35,7 +35,7 @@ export class ListeComponent implements OnInit {
       email: 'theo@example.com',
       phone: '07 45 67 89 01',
       skills: ['Ruby on Rails', 'RSpec', 'PostgreSQL'],
-      image: 'https://example.com/theo_profile.jpg'
+      image: 'https://media.licdn.com/dms/image/C4E03AQF1BSXKF_DQwA/profile-displayphoto-shrink_800_800/0/1634910992501?e=2147483647&v=beta&t=ccAaW3QlmKoA-w7LOjkYK287g4Z-RgsWDFdnfyvF910'
     },
     {
       id: 4,
@@ -43,7 +43,7 @@ export class ListeComponent implements OnInit {
       email: 'lea@example.com',
       phone: '06 56 78 90 12',
       skills: ['Django', 'REST framework', 'Celery'],
-      image: 'https://example.com/lea_profile.jpg'
+      image: 'https://th.bing.com/th/id/OIP.eiuiQ3_r8dM43pei_5-2IgAAAA?w=177&h=180&c=7&r=0&o=5&pid=1.7'
     },
     {
       id: 5,
@@ -51,7 +51,7 @@ export class ListeComponent implements OnInit {
       email: 'hugo@example.com',
       phone: '07 67 89 01 23',
       skills: ['React.js', 'Redux', 'TypeScript'],
-      image: 'https://example.com/hugo_profile.jpg'
+      image: 'https://th.bing.com/th/id/OIP.IS4E2H45jHy7IDBQaMceUQHaHa?w=800&h=800&rs=1&pid=ImgDetMain'
     },
     {
       id: 6,
@@ -59,15 +59,7 @@ export class ListeComponent implements OnInit {
       email: 'manon@example.com',
       phone: '06 78 90 12 34',
       skills: ['Java', 'Spring Boot', 'Hibernate'],
-      image: 'https://example.com/manon_profile.jpg'
-    },
-    {
-      id: 7,
-      name: 'Maxime Girard',
-      email: 'maxime@example.com',
-      phone: '07 89 01 23 45',
-      skills: ['Angular', 'RxJS', 'NgRx'],
-      image: 'https://example.com/maxime_profile.jpg'
+      image: 'https://th.bing.com/th/id/R.e2796d879f5c9bea689ea9e6e35b0de4?rik=jkWJTG8SXTQFKw&riu=http%3a%2f%2fdiffusionph.cccommunication.biz%2fjpgok%2fRepGR%2f223%2f223174_73.jpg&ehk=FknchrnHGzSIsUpYTEdzszI%2byutz0zk7aORu4TBXA%2bQ%3d&risl=&pid=ImgRaw&r=0'
     }
  
 
